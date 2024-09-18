b784fb35dbe41db8876607022b07284d32308817
Sharrief Shabazz <sharrief.shabazz@gemini.com>
2024-09-18 00:10:13 +0000

    Add item detail and start signup form

--- learndjango/core/views.py
+++ learndjango/core/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 from item.models import Category, Item
 
+from .forms import SignupForm
+
 # Create your views here.
 def index(request):
   items = Item.objects.filter(is_sold=False)[0:6]
@@ -14,3 +16,9 @@ def index(request):
 
 def contact(request):
   return render(request, 'core/contact.html')
+
+def signup(request):
+  form = SignupForm()
+  return render(request, 'core/signup.html', {
+    'form': form
+  })
